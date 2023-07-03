66ec283ed3986290c2e49bf0472055ef44608fb6
girey0211 <kkswin1234@gmail.com>
2023-07-03 11:31:29 +0900

    ADD : chat item

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,14 @@
+import "./index.css";
+import "../../fixtures";
+import ChatItem from "../ChatItem";
+import { imageIcon } from "../../fixtures";
+
 const Main = () => {
-  return <div>Main</div>;
+    return (
+        <div className="main">
+            <ChatItem imageLink={imageIcon} text={"코딩 잘하는법"} />
+        </div>
+    );
 };
 
 export default Main;
---
+++ src/fixtures/index.js
@@ -0,0 +1,2 @@
+export const imageIcon =
+    "https://cdn.spotvnews.co.kr/news/photo/202303/595149_826741_4844.jpg";
